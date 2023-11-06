32c53bab7201d68c5b630f881212e8f1ce8f78a9
alextan030902 <144402649+alextan030902@users.noreply.github.com>
2023-11-06 09:46:08 +0800

    temp

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAwLgxI3Tcuf8Ql1tFuwdEiX8Eifb6HK3k",
+  authDomain: "crophub-506cc.firebaseapp.com",
+  databaseURL: "https://crophub-506cc-default-rtdb.firebaseio.com",
+  projectId: "crophub-506cc",
+  storageBucket: "crophub-506cc.appspot.com",
+  messagingSenderId: "1098592905964",
+  appId: "1:1098592905964:web:96da39a9604a80a1ad93c9",
+  measurementId: "G-68M4XREBPX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => 
@@ -36,11 +58,39 @@ document.querySelector('#menu-btn').onclick = () =>
   searchForm.classList.remove('active');
 }
 
-document.onscroll= () => 
+window.onscroll= () => 
 {
   searchForm.classList.remove('active');
   shoppingCart.classList.remove('active');
   loginForm.classList.remove('active');
   navbar.classList.remove('active');
 }
+
+const forms = document.querySelector(".forms"),
+pwShowHide = document.querySelectorAll(".eye-icon"),
+links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+eyeIcon.addEventListener("click", () => {
+  let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+  
+  pwFields.forEach(password => {
+      if(password.type === "password"){
+          password.type = "text";
+          eyeIcon.classList.replace("bx-hide", "bx-show");
+          return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+  })
+  
+})
+})      
+
+links.forEach(link => {
+link.addEventListener("click", e => {
+ e.preventDefault(); //preventing form submit
+ forms.classList.toggle("show-signup");
+})
+})
 
