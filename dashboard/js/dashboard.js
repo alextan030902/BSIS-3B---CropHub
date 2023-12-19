f6e4e3d8dc110c29e8e8f0ea104d9c6f26c8a299
alextan030902 <144402649+alextan030902@users.noreply.github.com>
2023-12-19 18:20:21 +0800

    updt

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
+import { getDatabase, ref, onValue, orderByChild, equalTo } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -26,7 +26,6 @@ const usersRef = ref(db, 'users');
 // Get references to HTML elements where you want to display the counts
 const totalProductsElement = document.getElementById('productsAdded');
 const totalOrdersElement = document.getElementById('totalOrders');
-const  totalPendingElement = document.getElementById('totalPending');
 const totalMessagesElement = document.getElementById('totalMessages');
 const totalUsersElement = document.getElementById('totalUsers');
 
@@ -46,14 +45,6 @@ onValue(ordersRef, (snapshot) => {
   totalOrdersElement.innerText = `${totalOrders}`;
 });
 
-// Listen for changes to the 'orders' node
-onValue(ordersRef, (snapshot) => {
-  let totalPending = snapshot.val() ? Object.keys(snapshot.val()).length : 0;
-
-  // Update the HTML with the total number of orders
-  totalPendingElement.innerText = `${totalPending}`;
-});
-
 // Listen for changes to the 'messages' node
 onValue(messagesRef, (snapshot) => {
   let totalMessages = snapshot.val() ? Object.keys(snapshot.val()).length : 0;
@@ -69,3 +60,33 @@ onValue(usersRef, (snapshot) => {
   // Update the HTML with the total number of users
   totalUsersElement.innerText = `${totalUsers}`;
 });
+
+const totalCompletedElement = document.getElementById('totalCompleted');
+const totalPendingElement = document.getElementById('totalPending');
+
+// Listen for changes to the 'orders' node
+onValue(ordersRef, (snapshot) => {
+  const orders = snapshot.val() || {};
+  
+  // Filter completed orders
+  const completedOrders = Object.values(orders).filter(order => order.status === 'completed');
+  const completedOrderCount = completedOrders.length;
+
+  // Filter pending orders
+  const pendingOrders = Object.values(orders).filter(order => order.status !== 'completed');
+  const pendingOrderCount = pendingOrders.length;
+
+  // Update the HTML with the total number of completed and pending orders
+  totalCompletedElement.innerText = `${completedOrderCount}`;
+  totalPendingElement.innerText = `${pendingOrderCount}`;
+});
+
+function signOut() {
+  // Perform any sign-out logic here (e.g., clearing session, etc.)
+
+  // Redirect to the login page
+  window.location.href = "login.html";
+}
+
+// Attach click event listener to the link
+document.getElementById("signOutLink").addEventListener("click", signOut);
--- dashboard/js/products.js
+++ dashboard/js/products.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase, ref, set, push, onValue } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-
+import { getDatabase, ref, set, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,113 +17,106 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();  // Use getDatabase to get a reference to the database
 
-
 function displayAllProducts() {
     const productsContainer = document.getElementById('products-container');
-  
+
     // Check if the container exists
     if (!productsContainer) {
-      console.error("Products container not found");
-      return;
+        console.error("Products container not found");
+        return;
     }
-  
+
     const productsRef = ref(db, "products");
-  
+
     onValue(productsRef, (snapshot) => {
-      // Clear the container
-      productsContainer.innerHTML = "";
+        // Clear the container
+        productsContainer.innerHTML = "";
 
-      // Create a single row to hold all product columns
-    const row = document.createElement('div');
-    row.classList.add('row', 'g-4');
-  
-      snapshot.forEach((userSnapshot) => {
-        const userData = userSnapshot.val();
-        const productID = userSnapshot.key;
-        const productName = userData.name;
-        const productPrice = userData.price;
-        const productImage = userData.image;
-        const productDescription = userData.description; // assuming description exists   
-       
-        
-         // Create a column for each product card
-      const col = document.createElement('div');
-      col.classList.add('col-md-4'); // Adjust the column size as needed
+        // Create a single row to hold all product columns
+        const row = document.createElement('div');
+        row.classList.add('row', 'g-4');
 
-      // Create and populate HTML elements for each product card
-      const cardElement = document.createElement('div');
-      cardElement.classList.add('card');
+        snapshot.forEach((userSnapshot) => {
+            const userData = userSnapshot.val();
+            const productID = userSnapshot.key;
+            const productName = userData.name;
+            const productPrice = userData.price;
+            const productImage = userData.image;
+            const productDescription = userData.description;
 
-    //Create and populate HTML elements
-      cardElement.classList.add('col');
-      cardElement.classList.add('card');
+            // Create a column for each product card
+            const col = document.createElement('div');
+            col.classList.add('col-md-4'); // Adjust the column size as needed
 
-      const imageElement = document.createElement('img');
-      imageElement.src = productImage;
-      imageElement.classList.add('card-img-top');
-      imageElement.alt = productName;
+            // Create and populate HTML elements for each product card
+            const cardElement = document.createElement('div');
+            cardElement.classList.add('card', 'col');
 
-      const cardBody = document.createElement('div');
-      cardBody.classList.add('card-body');
+            const imageElement = document.createElement('img');
+            imageElement.src = productImage;
+            imageElement.classList.add('card-img-top');
+            imageElement.alt = productName;
 
-      const cardTitle = document.createElement('h5');
-      cardTitle.classList.add('card-title', 'text-capitalize');
-      cardTitle.textContent = productName;
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
 
-      const cardDescription = document.createElement('p');
-      cardDescription.classList.add('card-description');
-      cardDescription.textContent = `${productDescription}`;
+            const cardTitle = document.createElement('h5');
+            cardTitle.classList.add('card-title', 'text-capitalize');
+            cardTitle.textContent = productName;
 
+            const cardDescription = document.createElement('p');
+            cardDescription.classList.add('card-description');
+            cardDescription.textContent = `${productDescription}`;
 
-      const cardText = document.createElement('p');
-      cardText.classList.add('card-text');
-      cardText.textContent = `(Price: $${productPrice})`;
+            const cardText = document.createElement('p');
+            cardText.classList.add('card-text');
+            cardText.textContent = `(Price: $${productPrice})`;
 
-      const buttonGroup = document.createElement('div');
-      buttonGroup.classList.add('d-grid', 'gap-2', 'd-md-block');
+            const buttonGroup = document.createElement('div');
+            buttonGroup.classList.add('d-grid', 'gap-2', 'd-md-block');
 
-      const editButton = document.createElement('a');
-      editButton.href = 'edit-product.html';
-      editButton.classList.add('btn', 'btn-primary', 'me-2');
-      editButton.textContent = 'Edit';
+            const editButton = document.createElement('a');
+            editButton.href = `edit-product.html?productId=${productID}`;
+            editButton.classList.add('btn', 'btn-primary', 'me-2');
+            editButton.textContent = 'Edit';
 
-      const deleteButton = document.createElement('button');
-      deleteButton.classList.add('btn', 'btn-danger');
-      deleteButton.textContent = 'Delete';
+            const deleteButton = document.createElement('button');
+            deleteButton.classList.add('btn', 'btn-danger');
+            deleteButton.textContent = 'Delete';
 
-      // Append elements to the card
-      cardBody.appendChild(cardTitle);
-      cardBody.appendChild(cardText);
-      cardBody.appendChild(buttonGroup);
+            // Add an event listener to the delete button
+            deleteButton.addEventListener('click', () => {
+                deleteProduct(productID);
+            });
 
-      buttonGroup.appendChild(editButton);
-      buttonGroup.appendChild(deleteButton);
+            // Append elements to the card
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText);
+            cardBody.appendChild(buttonGroup);
 
-      cardElement.appendChild(imageElement);
-      cardElement.appendChild(cardBody);
+            buttonGroup.appendChild(editButton);
+            buttonGroup.appendChild(deleteButton);
 
-      // Add card to container
-      productsContainer.appendChild(cardElement);
+            cardElement.appendChild(imageElement);
+            cardElement.appendChild(cardBody);
 
-     
-      col.appendChild(cardElement);
+            // Add card to container
+            productsContainer.appendChild(cardElement);
 
-      // Append the column to the row
-      row.appendChild(col);
+            col.appendChild(cardElement);
 
-      });
-       // Append the row to the products container
-    productsContainer.appendChild(row);
+            // Append the column to the row
+            row.appendChild(col);
+        });
+
+        // Append the row to the products container
+        productsContainer.appendChild(row);
     });
-  }
-  
-  window.addEventListener('load', displayAllProducts);
-  
+}
 
+window.addEventListener('load', displayAllProducts);
 
-
-
-
-   
-
-
+function deleteProduct(productId) {
+    const productsRefToDelete = ref(db, `products/${productId}`);
+    remove(productsRefToDelete);
+}
